357d4b18276e319f93a9bbaf491dfa4e6eca51db
alessandrostein <alessandro@alessandrostein.com>
2018-08-17 19:24:22 -0300

    Specific router for sale

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "sales#index"
+  resources :sales, except: [:edit, :destroy] do
+    collection do
+      post :import
+    end
+  end
 end
