d094f60ca4b56ce9ec3b89e5fa448f0f29658819
alessandrostein <alessandro@alessandrostein.com>
2018-08-20 10:06:11 -0300

    Useful methods

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,20 @@ class Sale < ActiveRecord::Base
   has_many :items, class_name: 'SaleItem'
 
   default_scope { order(created_at: :desc) }
+
+  def total_price
+    self.items.map { |i| i.total_price }.sum
+  end
+
+  def total_amount
+    self.items.sum(:amount)
+  end
+
+  def total_items
+    self.items.count
+  end
+
+  def self.total_sales
+    Sale.all.count
+  end
 end
--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -1,3 +1,19 @@
 class SaleItem < ActiveRecord::Base
   belongs_to :sale
+
+  def total_price
+    self.price * self.amount
+  end
+
+  def self.total_items
+    SaleItem.all.count
+  end
+
+  def self.total_amount
+    SaleItem.sum(:amount)
+  end
+
+  def self.total_price
+    SaleItem.all.map { |i| i.total_price }.sum
+  end
 end
