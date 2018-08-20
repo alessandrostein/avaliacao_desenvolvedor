a84f10ddc313f792e3993b24bc6b4c34df47c897
alessandrostein <alessandro@alessandrostein.com>
2018-08-20 00:49:12 -0300

    Notification message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def notification
+    messages = ""
+    [:success, :danger].each {|type|
+      if flash[type]
+        messages += "<div class='alert alert-#{type}' role='alert'>#{flash[type]}</div>"
+      end
+    }
+
+    messages.html_safe
+  end
 end
