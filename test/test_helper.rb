7897c000d93d297ca84385ec8a6a951d479becd4
alessandrostein <alessandro@alessandrostein.com>
2018-08-20 10:06:59 -0300

    Useful test helper for items

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,23 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def default_items_creator
+    sale = Sale.create
+    sale.items << SaleItem.new(items_params)
+    sale.items << SaleItem.new(items_params)
+    sale.save
+    sale
+  end
+
+  def items_params
+    {
+      buyer: 'Buyer',
+      description: 'Description',
+      price: 10,
+      amount: 2,
+      address: 'Address',
+      vendor: 'Vendor'
+    }
+  end
 end
