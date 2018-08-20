9f382e624087c4f2f84f30ec3a76f8b68682ce2e
alessandrostein <alessandro@alessandrostein.com>
2018-08-20 10:07:15 -0300

    Sales Controller test

---
+++ test/controllers/sales_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class SalesControllerTest < ActionController::TestCase
+  setup do
+    @sale = sales(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:sales)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show, params: { id: @sale }
+    assert_response :success
+  end
+end
